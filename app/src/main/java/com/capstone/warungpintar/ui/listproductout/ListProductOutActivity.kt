8dbbac0d60554461e735d374e366a11ba4983bb2
Muhammad Hadi Royan Firdaus <hadiroyan3@gmail.com>
2023-12-14 21:45:52 +0700

    create adapter and fix UI for list product out

--- app/src/main/java/com/capstone/warungpintar/ui/listproductout/ListProductOutActivity.kt
+++ app/src/main/java/com/capstone/warungpintar/ui/listproductout/ListProductOutActivity.kt
@@ -1,12 +1,30 @@
 package com.capstone.warungpintar.ui.listproductout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.capstone.warungpintar.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.capstone.warungpintar.databinding.ActivityListProductOutBinding
 
 class ListProductOutActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityListProductOutBinding
+    private lateinit var adapter: ListProductOutAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_product_out)
+        binding = ActivityListProductOutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        adapter = ListProductOutAdapter()
+        with(binding) {
+            // setup top bar
+            topAppBar.setNavigationOnClickListener {
+                onBackPressedDispatcher.onBackPressed()
+            }
+            
+            // setup recycler view
+            listItem.layoutManager = LinearLayoutManager(this@ListProductOutActivity)
+            listItem.adapter = adapter
+        }
+
     }
 }
