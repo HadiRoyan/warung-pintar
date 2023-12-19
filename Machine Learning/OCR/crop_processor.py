e1a54eed4d9b840a4088951849b187415977fadb
Mikejd3 <81763848+Mikejd3@users.noreply.github.com>
2023-12-19 19:27:49 +0700

    Update crop_processor.py

--- Machine Learning/OCR/crop_processor.py
+++ Machine Learning/OCR/crop_processor.py
@@ -1,81 +1,91 @@
-import os
-import shutil
-import tensorflow as tf
-from google.colab.patches import cv2_imshow
-import cv2
-from imutils import contours
-from skimage.transform import resize
-from PIL import Image
-import numpy as np
+from fastapi import FastAPI
 
-#Load model
-loaded_model = tf.keras.models.load_model("./model_ocr.h5")
+app = FastAPI()
+@app.get("/")
+def crop_processor(model_path, image_path):
+  import os
+  import shutil
+  from tensorflow.keras.preprocessing.image import ImageDataGenerator
+  import tensorflow as tf
+  #from google.colab.patches import cv2_imshow
+  import cv2
+  from imutils import contours
+  from skimage.transform import resize
+  from PIL import Image
+  import numpy as np
+  
+  #Load model
+  loaded_model = tf.keras.models.load_model(model_path)
 
-# Load image and crop to desired guideline
-image = Image.open('./download.jpg') #Input
+  # Load image and crop to desired guideline
+  image = Image.open(image_path) #Input
 
-width, height = image.size 
-new_width = 350
-new_height = 200
+  width, height = image.size 
+  new_width = 350
+  new_height = 200
 
-left = (width/2 - new_width)
-top = (height/2 - new_height)
-right = (width/2 + new_width)
-bottom = (height/2 + new_height)
+  left = (width/2 - new_width)
+  top = (height/2 - new_height)
+  right = (width/2 + new_width)
+  bottom = (height/2 + new_height)
 
-# Crop the center of the image
-image = image.crop((left, top, right, bottom))
+  # Crop the center of the image
+  image = image.crop((left, top, right, bottom))
 
-#Show the image
-#plt.imshow(image)
-#plt.axis('off')
-#plt.show()
+  #Show the image
+  #plt.imshow(image)
+  #plt.axis('off')
+  #plt.show()
 
-# grayscale and Otsu's threshold
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-thresh = cv2.threshold(gray,0,255,cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)[1]
+  # grayscale and Otsu's threshold
+  image = np.array(image)
+  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  thresh = cv2.threshold(gray,0,255,cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)[1]
 
-# Find contours, sort from left-to-right, then crop
-cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-cnts, _ = contours.sort_contours(cnts, method="left-to-right")
+  # Find contours, sort from left-to-right, then crop
+  cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+  cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+  cnts, _ = contours.sort_contours(cnts, method="left-to-right")
 
-ROI_number = 0
-# Making a directory for the crop
-CROP_DIR = './crop'
-os.makedirs(CROP_DIR)
-# Cropping image
-for c in cnts:
-    area = cv2.contourArea(c)
-    if area > 10:
-        x,y,w,h = cv2.boundingRect(c)
-        ROI = 255 - image[y:y+h, x:x+w]
-        cv2.imwrite(os.path.join(CROP_DIR,'ROI_{}.png'.format(ROI_number)), ROI)
-        cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
-        ROI_number += 1
+  ROI_number = 0
+  # Making a directory for the crop
+  CROP_DIR = './crop'  
+  os.makedirs(CROP_DIR)
+  # Cropping image
+  for c in cnts:
+      area = cv2.contourArea(c)
+      if area > 10:
+          x,y,w,h = cv2.boundingRect(c)
+          ROI = 255 - image[y:y+h, x:x+w]
+          cv2.imwrite(os.path.join(CROP_DIR,'ROI_{}.png'.format(ROI_number)), ROI)
+          cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
+          ROI_number += 1
 
 
-result_prediction= ""
+  result_prediction= ""
 
-# Predicting each of the cropped image
+  # Predicting each of the cropped image
 
-for num in range(0,len(os.listdir(CROP_DIR))):
-  test_image = tf.keras.utils.load_img("ROI_" + str(num)+ ".png")
+  for num in range(0,len(os.listdir(CROP_DIR))):
+    test_image = tf.keras.utils.load_img("./crop/ROI_" + str(num)+ ".png")
 
-  # Resizing and formating
-  test_image_array = tf.keras.utils.img_to_array(test_image)
-  #print(f"Image (before) has shape: {test_image_array.shape}")
-  test_array_corrected = resize(test_image_array, (32, 32)).astype(np.uint8)
-  #print(f"Image (after) has shape: {test_array_corrected.shape}")
-  test_input = np.array([test_array_corrected])
+    # Resizing and formating
+    test_image_array = tf.keras.utils.img_to_array(test_image)
+    #print(f"Image (before) has shape: {test_image_array.shape}")
+    test_array_corrected = resize(test_image_array, (32, 32)).astype(np.uint8)
+    #print(f"Image (after) has shape: {test_array_corrected.shape}")
+    test_input = np.array([test_array_corrected])
 
-  # Predicting
-  predict_datagen = ImageDataGenerator(rescale = 1.0/255.)
-  predict_generator = predict_datagen.flow(test_input, batch_size=1)
-  prediction = loaded_model.predict(predict_generator,verbose = 0 )
+    # Predicting
+    predict_datagen = ImageDataGenerator(rescale = 1.0/255.)
+    predict_generator = predict_datagen.flow(test_input, batch_size=1)
+    prediction = loaded_model.predict(predict_generator,verbose = 0 )
 
-  # Printing result
-  result_prediction = result_prediction + "".join(str(x) for x in np.argmax(prediction, axis=1))
+    # Printing result
+    result_prediction = result_prediction + "".join(str(x) for x in np.argmax(prediction, axis=1))
 
-shutil.rmtree(CROP_DIR) #Deleting the crop directory since not used anymore
-print(result_prediction)
+  shutil.rmtree(CROP_DIR) #Deleting the crop directory since not used anymore
+  print(result_prediction)
+
+  return result_prediction
+
